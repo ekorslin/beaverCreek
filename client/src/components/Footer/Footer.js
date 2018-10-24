e51c6580625953a8c9aef92acef7ef1d5d45dcf6
Joseph Perkins <gamerdew64@gmail.com>
2018-10-24 13:53:03 -0500

    fixed footer along bottom

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import "./Footer.css";
 
 function Footer(/*props*/) {
   return (
-    <footer class="navbar-dark bg-dark" id="footerSection">
+    <footer class="navbar-dark fixed-bottom bg-dark" id="footerSection">
       <div class="container">
         <div class="row">
           <div class="col-lg-3 col-md-6 text-center">
