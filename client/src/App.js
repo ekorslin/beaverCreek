32886fdea67573cca783e9f8ee8036607a298a0f
Joseph Perkins <gamerdew64@gmail.com>
2018-10-24 15:20:19 -0500

    added icon next to Beaver Creek navbar, and added icons to footer

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ import Wrapper from "./components/Wrapper";
 import Time from "./components/timeSelect";
 import Home from "./components/home";
 import Form from "./components/form";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faEnvelope, faKey);
 
 const App = () => (
  <Router>
