25f735a0a6768775ce7f4e9746f274d056eb8771
Joseph Perkins <gamerdew64@gmail.com>
2018-10-25 15:45:17 -0500

    added background image to form timeSelect and daySelect components

--- client/src/components/daySelect/daySelect.js
+++ client/src/components/daySelect/daySelect.js
@@ -18,15 +18,25 @@ class Book extends Component {
   render() {
     return (
       <div>
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8"><br/>
-            <h2 className="mbl text-center">Select a Day</h2>
-            <ReactWeeklyDayPicker mobilView={window.innerWidth < 100} format={'YYYY-MM-DD'} selectDay={this.onClick} />
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+            <img className="d-block w-100 img-fluid" src="./put.jpg" alt="First slide"/>
+              <div className="carousel-caption d-none d-md-block">
+                <div>
+                  <div className="row">
+                    <div className="col-md-2"></div>
+                    <div className="col-md-8"><br/>
+                      <h2 className="mbl text-center">Select a Day</h2>
+                      <ReactWeeklyDayPicker mobilView={window.innerWidth < 100} format={'YYYY-MM-DD'} selectDay={this.onClick} />
+                    </div>
+                    <div className="col-md-2"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-md-2"></div>
         </div>
-
       </div>
     );
   }
