0f1a28e75883c6e2777ab299bdebaada455da5d9
Joseph Perkins <gamerdew64@gmail.com>
2018-11-10 09:21:39 -0600

    updated styling in client and config folders

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@
 // Passport operates on server as a gatekeeper. When you login it fiters out who is a real user or not. For any other protected route it will do the same thing.
 // Implemented JSON web token, which is an ID that can be used instead of user name and password each time.
 // When you login with the correct credientials, you are sent a token instead of cookies after credientials are validated.
-// Passport sends react a token that we are storing in session storage (outside of the react ecosystem). When you log out, the token is deleted from session storage.
+// Passport sends react a token that we are storing in session storage (outside of the react ecosystem). When you log out, the token is deleted from session storage
 
 // Requiring passport, passport and passport-local, and passport-jwt (so that we can use JSON tokens for authenticaion rather than cookies)
 var passport = require("passport");
